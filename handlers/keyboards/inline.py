9af4abdad700bee54eac7d848da3e0c566352aa9
themirjalol <mirjalolabdisamat@gmail.com>
2025-07-18 05:36:29 +0500

    Delete handlers/keyboards directory

--- handlers/keyboards/inline.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from config import CHANNEL_ID
-
-def check_subscribe_keyboard():
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="✅ Obuna bo‘ldim", callback_data="check_sub")],
-        [InlineKeyboardButton(text="🔗 Kanal", url=f"https://t.me/{CHANNEL_ID.lstrip('@')}")]
-    ])
